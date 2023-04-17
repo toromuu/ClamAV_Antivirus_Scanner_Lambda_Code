e493c78bceece5dc913b0510df184a20dd405dd8
toromuu <diegotoro1998@gmail.com>
2023-04-17 13:33:39 +0200

    [ADD] Github Actions CI

--- src/index.js
+++ src/index.js
@@ -1,107 +1,90 @@
-const NodeClam = require("clamscan");
-const fs = require("fs");
-const AWS = require("aws-sdk");
-const path = require("path");
+const NodeClam = require("clamscan");  // Import NodeClam module for virus scanning
+const fs = require("fs");  // Import fs module for file system operations
+const AWS = require("aws-sdk");  // Import AWS SDK for S3 operations
+const path = require("path");  // Import path module for file path operations
 
-const region = process.env.AWS_REGION;
-const quarantine_bucket = process.env.S3_BUCKET_QUARANTINE;
+const region = process.env.AWS_REGION;  // Get AWS region from environment variable
+const production_bucket = process.env.S3_PRODUCTION_BUCKET;  // Get production or destionation S3 bucket from environment variable
 
-const s3 = new AWS.S3({
+const s3 = new AWS.S3({  // Initialize S3 client
   region: region,
 });
 
-const options = {
+const options = {  // Options for virus scanning
   preference: "clamscan",
 };
 
+// Lambda function handler for S3 events
 module.exports.handler = async (s3Event) => {
-  console.log(
-    "Received S3 event - handling virus scan",
-    JSON.stringify(s3Event)
-  );
+  console.log("Received S3 event - Handling virus scan", JSON.stringify(s3Event));
 
+  // Get source bucket and object key from S3 event
   const sourceBucket = s3Event.Records[0].s3.bucket.name;
-  const objectKey = decodeURIComponent(
-    s3Event.Records[0].s3.object.key.replace(/\+/g, " ")
-  );
+  const objectKey = decodeURIComponent(s3Event.Records[0].s3.object.key.replace(/\+/g, " "));
 
-  const objectToScan = await s3
-    .getObject({
-      Bucket: sourceBucket,
-      Key: objectKey,
-    })
-    .promise();
-
+  // Download object from S3 and save it to a localtemporal file
+  const objectToScan = await s3.getObject({
+    Bucket: sourceBucket,
+    Key: objectKey,
+  }).promise();
   const localPath = `/tmp/${objectKey}`;
-  // Create any necessary local folders
   const localDir = path.dirname(localPath);
   fs.mkdirSync(localDir, { recursive: true });
-
   fs.writeFileSync(`/tmp/${objectKey}`, objectToScan.Body);
   console.log(`File ${objectKey} written successfully\n`);
 
-  ClamScan = new NodeClam();
-
+  // Initialize virus scanner and perform scan
+  const ClamScan = new NodeClam();
   await ClamScan.init(options)
     .then(async (clamscan) => {
-      try {
-        console.log(clamscan);
-        const version = await clamscan.getVersion();
-        console.log(`ClamAV Version: ${version}`);
-        const { err, file, isInfected, viruses } = await clamscan.isInfected(
-          localPath
-        );
+     try {   
+      console.log(clamscan);
+      const version = await clamscan.getVersion();
+      console.log(`ClamAV Version: ${version}`);
+      const { err, file, isInfected, viruses } = await clamscan.isInfected(localPath);
 
-        // If malware is detected, move the object to the quarantine bucket
-        if (isInfected === true) {
-          await s3
-            .copyObject({
-              Bucket: quarantine_bucket,
-              CopySource: `${sourceBucket}/${objectKey}`,
-              Key: objectKey,
-            })
-            .promise();
-
-          const timestamp = new Date().toISOString();
-
-          await s3
-            .putObjectTagging({
-              Bucket: quarantine_bucket,
-              Key: objectKey,
-              Tagging: {
-                TagSet: [
-                  { Key: "Status", Value: "Dirty" },
-                  { Key: "ScanTimestamp", Value: timestamp },
-                  { Key: "Virus", Value: viruses.toString() },
+      // If malware is detected, maintain the object in the quarantine bucket
+      if (isInfected === true) {
+        console.log("INFECTED");
+        const timestamp = new Date().toISOString();
+        await s3.putObjectTagging({
+          Bucket: sourceBucket,
+          Key: objectKey,
+          Tagging: {
+            TagSet: [
+              { Key: "Status", Value: "INFECTED" },
+              { Key: "Timestamp", Value: timestamp },
+              { Key: "Virus", Value: viruses.toString() },
                 ],
               },
             })
             .promise();
-
-          await s3
-            .deleteObject({
-              Bucket: sourceBucket,
-              Key: objectKey,
-            })
-            .promise();
-
-          console.log(`Virus found, ${objectKey} removed from ${sourceBucket}`);
-        } else {
+          console.log(`Virus found, ${objectKey}`);
+      } else {
           // If no malware is detected, tag the object with the scan result and timestamp
-          console.log("CLEAN");
+          console.log("CLEAN");    
+          await s3.copyObject({
+            Bucket: production_bucket,
+            CopySource: `${sourceBucket}/${objectKey}`,
+            Key: objectKey,
+          }).promise();
           const timestamp = new Date().toISOString();
-          await s3
-            .putObjectTagging({
-              Bucket: sourceBucket,
-              Key: objectKey,
-              Tagging: {
-                TagSet: [
-                  { Key: "Status", Value: "Clean" },
-                  { Key: "ScanTimestamp", Value: timestamp },
-                ],
-              },
-            })
-            .promise();
+          // Tag the object with the scan result, virus found and timestamp
+          await s3.putObjectTagging({
+            Bucket: production_bucket,
+            Key: objectKey,
+            Tagging: {
+              TagSet: [
+                { Key: "Status", Value: "CLEAN" },
+                { Key: "Timestamp", Value: timestamp }
+              ],
+            },
+          }).promise();
+          // Delete object from the quarantine bucket
+          await s3.deleteObject({
+            Bucket: sourceBucket,
+            Key: objectKey,
+          }).promise();
         }
       } catch (err) {
         console.log("Errors have occurred during scanning: ");
